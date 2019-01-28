0c4cd1df85a300c8c29f51e3d66f4126e582c8ef
François Gallard <francois.gallard@irt-saintexupery.com>
2019-01-28 18:21:54 +0100

    Add a starting point for the GEMS interface

---
+++ python-wrapper/__init__.py
@@ -0,0 +1,9 @@
+# -*-mode: python; py-indent-offset: 4; tab-width: 8; coding:utf-8 -*-
+# Copyright (c) 2019 IRT-AESE
+# All rights reserved.
+#
+# Contributors:
+#    INITIAL AUTHORS - initial API and implementation and/or
+#                      initial documentation
+#        :author:  Francois Gallard
+#    OTHER AUTHORS   - MACROSCOPIC CHANGES
---
+++ python-wrapper/test_gems_wrapper.py
@@ -0,0 +1,31 @@
+# -*-mode: python; py-indent-offset: 4; tab-width: 8; coding:utf-8 -*-
+# Copyright (c) 2019 IRT-AESE
+# All rights reserved.
+#
+# Contributors:
+#    INITIAL AUTHORS - initial API and implementation and/or
+#                      initial documentation
+#        :author:  Francois Gallard
+#    OTHER AUTHORS   - MACROSCOPIC CHANGES
+
+import unittest
+from gems_wrapper import ToySphereDiscipline
+from numpy import ones
+
+
+class TestGEMSWrapper(unittest.TestCase):
+
+    def test_basic(self):
+        mesh_file = "../examples/data/unitSphere1.stl"
+        neutral_mesh_file = "../examples/data/unitSphere2.stl"
+        toy1 = ToySphereDiscipline("Sphere1", ["Forces"], ["Pressure"], mesh_file,
+                                   neutral_mesh_file, sphere_radius=1.0)
+
+        toy1.execute({"Forces": ones(1000)})
+
+        toy1.close()
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()

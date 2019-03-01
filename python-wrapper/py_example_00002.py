6de1c7680ad511e042f160f7daebac3846f26966
Marco Cisternino <marco.cisternino@optimad.it>
2019-03-01 12:43:43 +0100

    examples: modify py_example_00002.py to easily test new methods

--- python-wrapper/py_example_00002.py
+++ python-wrapper/py_example_00002.py
@@ -1,4 +1,5 @@
 import coupling
+import numpy as np
 inputs=["Forces"]
 outputs=["Pressure"]
 mc=coupling.Py_MeshCoupling(inputs,outputs)
@@ -8,6 +9,9 @@ mc.initialize(disciplineFile,neutralFile,2.0)
 nD=coupling.Py_PiercedVector()
 #mesh=coupling.Py_SurfUnstructured()
 mesh=mc.getNeutralMesh()
-coupling.Py_initDoubleDataOnMesh(mesh,nD)
+#coupling.Py_initDoubleDataOnMesh(mesh,nD)
+arr=np.ones(82)
+print "suca"
+coupling.Py_initDataOnMeshFromArray(mesh,nD,arr)
 mc.compute(nD)
 mc.close()

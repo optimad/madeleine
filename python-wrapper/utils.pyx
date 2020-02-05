e34da4ae3870eed9b33196ba62ae9f5720d409f0
Jean-Christophe Giret <jean-christophe.giret@irt-saintexupery.com>
2020-02-05 10:33:12 +0100

    Remove PiercedVector related code from the Cython wrapper

--- python-wrapper/utils.pyx
+++ python-wrapper/utils.pyx
@@ -1,4 +1,3 @@
-include "piercedVector.pyx"
 include "surfUnstructured.pyx"
 from libc.stdint cimport uintptr_t
 import numpy as np
@@ -8,27 +7,8 @@ from libcpp.vector cimport vector
 from libcpp.string cimport string
 
 cdef extern from "couplingUtils.hpp" namespace "coupling":
-    cdef void initDoubleDataOnMesh(const SurfUnstructured * mesh, PiercedVector[double,long] * data)
-    cdef void initDataOnMeshFromArray(const SurfUnstructured * mesh, PiercedVector[double,long] * data, double * arr, size_t arrSize)
-    cdef void moveDataOnMeshToArray(const SurfUnstructured * mesh, PiercedVector[double,long] * data, double * arr, size_t arrSize)
     cdef vector[int] computeMeshFilePartitioning(const string meshFile,MPI_Comm);
 
-    
-def Py_initDoubleDataOnMesh(Py_SharedSurfUnstructured mesh, Py_PiercedVector data):
-    initDoubleDataOnMesh(<SurfUnstructured*><void*>mesh.thisptr,<PiercedVector[double,long]*><void*>data.thisptr)
-
-def Py_initDataOnMeshFromArray(Py_SharedSurfUnstructured mesh, Py_PiercedVector data, arr):
-    if not arr.flags['C_CONTIGUOUS']:
-        arr = np.ascontiguousarray(arr)
-    cdef double[::1] arr_memview = arr
-    initDataOnMeshFromArray(<SurfUnstructured*><void*>mesh.thisptr,<PiercedVector[double,long]*><void*>data.thisptr,&arr_memview[0],arr.shape[0])
-     
-def Py_moveDataOnMeshToArray(Py_SharedSurfUnstructured mesh, Py_PiercedVector data, arr):
-    if not arr.flags['C_CONTIGUOUS']:
-        arr = np.ascontiguousarray(arr)
-    cdef double[::1] arr_memview = arr
-    moveDataOnMeshToArray(<SurfUnstructured*><void*>mesh.thisptr,<PiercedVector[double,long]*><void*>data.thisptr,&arr_memview[0],arr.shape[0])
-    
 def Py_computeMeshFilePartitioning(meshFile,MPI.Comm comm):
     cdef MPI_Comm mpi_comm
     cdef vector[int] cellIndicesPerRank

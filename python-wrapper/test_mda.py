71ed9536fbfcd42d3922a41d9e4fc21537da161d
Jean-Christophe Giret <jean-christophe.giret@irt-saintexupery.com>
2020-02-05 20:16:34 +0100

    Add test for MDA

---
+++ python-wrapper/test_mda.py
@@ -0,0 +1,61 @@
+# -*-mode: python; py-indent-offset: 4; tab-width: 8; coding:utf-8 -*-
+# Copyright (c) 2019 IRT-AESE
+# All rights reserved.
+#
+# Contributors:
+#    INITIAL AUTHORS - initial API and implementation and/or
+#                      initial documentation
+#        :author:  Francois Gallard
+#    OTHER AUTHORS   - MACROSCOPIC CHANGES
+
+import unittest
+from numpy import ones
+
+from gems.parallel.core.mpi_manager import MPIManager, get_world_comm
+from gems.parallel.mda.parallel_jacobi import ParallelMDAJacobi
+from gems_wrapper import ToySphereDiscipline
+
+COMM = get_world_comm()
+
+class TestGEMSWrapper(unittest.TestCase):
+
+    def test_basic(self):
+        MPIManager().clear(1)
+
+        mesh_file = "../examples/data/unitSphere5.stl"
+        neutral_mesh_file = "../examples/data/unitSphere4.stl"
+
+        toy1 = ToySphereDiscipline("Sphere1", ["Forces"], ["Pressure"], mesh_file,
+                                   neutral_mesh_file, sphere_radius=1.0,
+                                   n_cpus=2)
+
+        toy2 = ToySphereDiscipline("Sphere2", ["Pressure"], ["Forces"], mesh_file,
+                                   neutral_mesh_file, sphere_radius=1.0,
+                                   n_cpus=3)
+
+        disciplines = [toy1, toy2]
+
+        mda = ParallelMDAJacobi(
+            disciplines=disciplines,
+            name="mda",
+            max_mda_iter=200,
+            tolerance=1e-6,
+            acceleration=ParallelMDAJacobi.SECANT_ACCELERATION,
+        )
+
+        default_inputs = mda.default_inputs
+        inputs = None
+        if mda.execution_context.is_rank_on_mpi_group():
+            r = ones(1)
+            size_forces = default_inputs['Forces'].shape[0]
+            forces = ones(size_forces)*0.03
+            pressure = ones(size_forces)*0.05
+            inputs = {'r': r, 'Forces': forces, 'Pressure': pressure}
+
+        out = mda.execute(inputs)
+
+        if mda.execution_context.is_rank_on_mpi_group():
+            print out
+
+if __name__ == "__main__":
+    unittest.main()

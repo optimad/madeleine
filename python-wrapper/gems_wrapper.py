6c0ea3e01178116450d284d2b358bc0dfeac2d76
Jean-Christophe Giret <jean-christophe.giret@irt-saintexupery.com>
2020-02-05 15:39:07 +0100

    Fix gems wrapper
    The wrapper was bugged when used in a composite pattern (MDOChain or MDA)

--- python-wrapper/gems_wrapper.py
+++ python-wrapper/gems_wrapper.py
@@ -11,7 +11,8 @@ from numpy import ones
 
 from gems.core.discipline import MDODiscipline
 from gems.parallel.api import create_execution_context, create_user_partition
-import coupling 
+from gems.parallel.core.mpi_manager import get_world_comm
+import coupling
 
 
 class ToySphereDiscipline(MDODiscipline):
@@ -34,16 +35,29 @@ class ToySphereDiscipline(MDODiscipline):
         self.execution_context = create_execution_context(self, n_cpus)
 
         comm = self.execution_context.comm
-        cell_indices_per_rank = coupling.Py_computeMeshFilePartitioning(neutral_mesh_file,
-                                                                     comm)
-        local_size = cell_indices_per_rank.shape[0]
-        all_sizes = comm.allgather(local_size)
+
+        cell_indices_per_rank = 0
+        local_size = 0
+        all_sizes = None
+        if self.execution_context.is_alive():
+            cell_indices_per_rank = coupling.Py_computeMeshFilePartitioning(neutral_mesh_file,
+                                                                        comm)
+            local_indices = [i for i in cell_indices_per_rank if i == comm.rank]
+            local_size = len(local_indices)
+            all_sizes = comm.allgather(local_size)
+
+        # Broadcast all_sizes to all the ranks of comm_world
+        comm_world = get_world_comm()
+        root = self.execution_context.comm_world_ranks[0]
+        all_sizes = comm_world.bcast(all_sizes, root=root)
 
         obj_variables = [0] * n_cpus
         obj_variables[0] = 1
+
         variables_sizes = {inputs[0]: all_sizes,
                            outputs[0]: all_sizes,
                            "r": obj_variables}
+
         partition = create_user_partition(self.execution_context,
                                           variables_sizes)
 
@@ -51,14 +65,18 @@ class ToySphereDiscipline(MDODiscipline):
         # subcommunicator
         partition.initialize_default_inputs(1.)
 
-        if comm.rank == 0:
-            self.default_inputs['r'] = ones(1)*sphere_radius
+        if self.execution_context.is_alive():
+            if comm.rank == 0:
+                self.default_inputs['r'] = ones(1)*sphere_radius
 
-        self.mesh_coupling = coupling.Py_MeshCoupling(inputs, outputs, name,
-                                                      comm)
+        if self.execution_context.is_alive():
+            self.mesh_coupling = coupling.Py_MeshCoupling(inputs,
+                                                          outputs,
+                                                          name,
+                                                          comm)
 
-        self.mesh_coupling.initialize(mesh_file, neutral_mesh_file,
-                                      sphere_radius, cell_indices_per_rank)
+            self.mesh_coupling.initialize(mesh_file, neutral_mesh_file,
+                                          sphere_radius, cell_indices_per_rank)
 
     def _run(self):
         input_vector = self.local_data[self.inputs[0]]
@@ -68,9 +86,10 @@ class ToySphereDiscipline(MDODiscipline):
         r = comm.bcast(r, root=0)
 
         # self.mesh_coupling.set_radius(r)
-        self.mesh_coupling.compute(input_vector)
 
-        self.local_data[self.outputs[0]] = input_vector
+        self.mesh_coupling.compute(input_vector)
+        output = {self.outputs[0]: input_vector}
+        self.store_local_data(**output)
 
     def close(self):
         self.mesh_coupling.close()

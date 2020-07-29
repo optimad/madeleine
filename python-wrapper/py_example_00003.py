b6efe9ebd0a2ba2500602b25c317537f95a4a787
Marco Cisternino <marco.cisternino@optimad.it>
2020-07-29 11:43:54 +0200

    python-wrapper/example: change to outer discipline to have a well-posed elliptic problem

--- python-wrapper/py_example_00003.py
+++ python-wrapper/py_example_00003.py
@@ -2,8 +2,8 @@ import coupling
 import numpy as np
 import mpi4py
 from mpi4py import MPI
-inputs=["Flux"] #inner sphere
-outputs=["Temperture"] #inner sphere
+inputs=["Temperature"] #outer sphere
+outputs=["Flux"] #outer sphere
 comm = MPI.COMM_WORLD
 mc=coupling.Py_MeshCoupling(inputs, outputs, "InnerSphere", comm)
 disciplineFile = "../examples/data/unitSphere5.stl"
@@ -12,9 +12,10 @@ cellIndicesPerRank = coupling.Py_computeMeshFilePartitioning(neutralFile,comm)
 print(cellIndicesPerRank)
 sourceDirection = [1.0, 0.0, 0.0]
 npSourceDirection = np.asarray(sourceDirection)
-mc.initialize(disciplineFile, neutralFile, 2.0, 2.0, 1.0, True, 1.0, npSourceDirection, cellIndicesPerRank,0)
+mc.initialize(disciplineFile, neutralFile, 2.0, 2.0, 1.0, False, 1.0, npSourceDirection, cellIndicesPerRank,0)
 nofNeutralElements=mc.getNeutralMeshSize()
 arr=np.ones(nofNeutralElements)
-mc.compute(arr,1.0,2.0)
+arr=arr * 274.0
+mc.compute(arr,2.0,2.0)
 print(arr)
 mc.close()

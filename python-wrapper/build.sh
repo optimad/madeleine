eb21ed2e06ea579285182fe6ca2f2572eac972b7
Jean-Christophe Giret <jean-christophe.giret@irt-saintexupery.com>
2020-12-01 12:24:26 +0100

    Update build.sh for JC, but should be more generic.

--- python-wrapper/build.sh
+++ python-wrapper/build.sh
@@ -1,1 +1,2 @@
-python setup.py build_ext --inplace --bitpit-path=/home/jc.giret/bitpit/ --madeleine-path=/home/jc.giret/SOURCES/madeleine_parallel/build/src/ --extensions-source=coupling.pyx  --metis-path=/home/jc.giret/.local/lib/ --lapack-path=/home/jc.giret/.conda/envs/madeleine_py38/lib/ --mpi-include-path=/home/jc.giret/.conda/envs/madeleine_py38/lib/ --petsc-path=/home/jc.giret/.conda/envs/madeleine_py38/lib/
+CONDA_ENV_PATH=/opt/anaconda/miniconda3/envs/test_cython/lib/
+python setup.py build_ext --inplace --bitpit-path=/home/jc.giret/bitpit/ --madeleine-path=/home/jc.giret/SOURCES/madeleine_parallel/build/src/ --extensions-source=coupling.pyx  --metis-path=/home/jc.giret/.local/lib/ --lapack-path=$CONDA_ENV_PATH --mpi-include-path=$CONDA_ENV_PATH --petsc-path=$CONDA_ENV_PATH
